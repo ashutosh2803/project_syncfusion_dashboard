db8d6195af738268208c3b115bd39f7602b66cf6
unknown <ashuisalive@gmail.com>
2023-03-08 02:07:25 +0530

    initial tailwind, syncfusion, folder structure created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-function App() {
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from "./pages";
+
+import "./App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div>
+            <div>
+              <Routes>
+                {/* dashboard  */}
+                <Route path="/" element="" />
+                <Route path="/ecommerce" element="" />
+
+                {/* pages  */}
+                <Route path="/orders" element="" />
+                <Route path="/employees" element="" />
+                <Route path="/customers" element="" />
+
+                {/* apps  */}
+                <Route path="/kanban" element="" />
+                <Route path="/editor" element="" />
+                <Route path="/calendar" element="" />
+                <Route path="/color-picker" element="" />
+
+                {/* charts  */}
+                <Route path="/line" element="" />
+                <Route path="/area" element="" />
+                <Route path="/bar" element="" />
+                <Route path="/pie" element="" />
+                <Route path="/financial" element="" />
+                <Route path="/color-mapping" element="" />
+                <Route path="/pyramid" element="" />
+                <Route path="/stacked" element="" />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
